594ab6ea038b4042e867d3a6cc9e7baea7c85d27
youssefAshraf21 <ya614962@gmail.com>
2026-05-10 14:11:55 +0300

    refactor: clean up Navbar component by removing unused code and improving structure

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -23,25 +23,6 @@ function Navbar() {
     return () => window.removeEventListener('scroll', onScroll);
   }, []);
 
-<<<<<<< Updated upstream
-    return (
-        <nav className="bg-black relative">
-            <div className='flex justify-between items-center bg-black p-5 max-w-[1400px] mx-auto'>
-                
-                <h1 className="text-white text-3xl font-black italic tracking-tighter uppercase z-50 ">
-                    <span className="text-orange-500"> Auto</span>Drive
-                </h1>
-                
-                
-                <ul className="hidden md:flex ml-auto gap-20 mr-15">
-                    {ListItems.map((item) => (
-                        <li key={item} className="relative group cursor-pointer text-white hover:text-orange-500 transition-colors duration-300 ">
-                            {item}
-                            <span className="absolute -bottom-1 left-0 w-0 h-[2px] bg-orange-500 transition-all duration-300 group-hover:w-full"></span>
-                        </li>
-                    ))}
-                </ul>
-=======
   const handleSearch = (e: React.FormEvent) => {
     e.preventDefault();
     if (search.trim()) {
@@ -50,7 +31,6 @@ function Navbar() {
       setNav(false);
     }
   };
->>>>>>> Stashed changes
 
   return (
     <nav
